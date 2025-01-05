ef508ba3895c0a44717c62d6cc923f13e07e6928
GJX <1393429978@qq.com>
2025-01-05 17:02:59 +0800

    rename

---
+++ run-ovmf/hda-contents/rookie.c
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    // 动态分配内存
+    int* p = (int *)malloc(10 * sizeof(int)); // 分配一个包含 10 个整数的数组
+    // 初始化分配的内存
+    for (int i = 0; i < 10; ++i) {
+        p[i] = i;
+    }
+
+    printf("Pointer p: %p\n", (void*)p);
+
+    int d;
+    for (int i = 0; i < 10; ++i) {
+        printf("%d ", p[i]);
+    }
+    printf("\n");
+
+    // 清理动态分配的内存
+    free(p);
+
+    return 0;
+}
+
