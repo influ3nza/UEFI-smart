b5569e7cb6638b2a306aa7f3584ab414ac142c44
Akatsuki Houkou <influenza-lv3@sjtu.edu.cn>
2025-01-05 15:30:39 +0800

    fix: v2p

--- run-ovmf/hda-contents/v2p.c
+++ run-ovmf/hda-contents/v2p.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdint.h>
 #include <string.h>
+#include <fcntl.h>
 
 #define PAGEMAP_ENTRY 8
 #define GET_BIT(X,Y) ((X & ((uint64_t)1<<Y)) >> Y)
@@ -11,26 +12,35 @@
 
 const int __endian_bit = 1;
 #define is_bigendian() ( (*(char*)&__endian_bit) == 0 )
+#define VAR_PATH "/sys/firmware/efi/efivars/MyVariable-a3a56e56-1d23-06dc-24bf-1473ff54e629"
 
 int i, c, pid, status;
 unsigned long virt_addr;
+int data_size;
 uint64_t read_val, file_offset, page_size;
 char path_buf [0x100] = {};
 FILE * f;
 char *end;
+uint64_t phys_addr;
+
+enum CMD_TYPE {
+    CMD_READ,
+    CMD_WRITE
+};
 
 int read_pagemap(char * path_buf, unsigned long virt_addr);
+int write_var(enum CMD_TYPE type);
 
 int main(int argc, char ** argv){
-    if(argc != 3){
-        printf("Argument number is not correct! It must like:\n./VtoP PID VIRTUAL_ADDRESS\n");
+    if(argc != 4){
+        printf("Argument number is not correct! It must like:\n./VtoP PID VIRTUAL_ADDRESS DATA_SIZE\n");
         return -1;
     }
-    if(!memcmp(argv[1], "self", sizeof("self"))){  //该VtoP进程自身
+    if(!memcmp(argv[1], "self", sizeof("self"))){
         sprintf(path_buf, "/proc/self/pagemap");
         pid = -1;
     }
-    else{  //指定的进程
+    else{
         pid = strtol(argv[1], &end, 10);
         if (end == argv[1] || *end != '\0' || pid <= 0){
             printf("PID must be a positive number or 'self'\n");
@@ -41,8 +51,12 @@ int main(int argc, char ** argv){
     if(pid != -1)
         sprintf(path_buf, "/proc/%u/pagemap", pid);
 
-    page_size = getpagesize();  //获取页面大小
-    read_pagemap(path_buf, virt_addr);  //读取页面映射内容
+    data_size = strtol(argv[3], NULL, 10);
+
+    page_size = getpagesize();
+    read_pagemap(path_buf, virt_addr);
+
+    write_var(CMD_READ);
     return 0;
 }
 
@@ -54,10 +68,6 @@ int read_pagemap(char * path_buf, unsigned long virt_addr){
         return -1;
     }
 
-    /* 
-     * 根据用户提供的虚拟内存地址计算该地址在文件中的偏移地址，公式为：
-     * 文件中偏移地址 = virt_addr偏移的字节数 * pagemap文件中条目的大小
-     */ 
     file_offset = virt_addr / page_size * PAGEMAP_ENTRY;
     printf("Vaddr: 0x%lx, Page_size: %lld, Entry_size: %d\n", virt_addr, page_size, PAGEMAP_ENTRY);
     printf("Reading %s at 0x%llx\n", path_buf, (unsigned long long) file_offset);
@@ -84,17 +94,58 @@ int read_pagemap(char * path_buf, unsigned long virt_addr){
     }
     printf("\n");
     printf("Result: 0x%llx\n", (unsigned long long) read_val);
-    /*
-     * 如果页面不存在，但在交换中，那么PFN包含交换文件编号的编码以及页面在交换中的偏移量。
-     * 未映射的页返回空的PFN。这允许精确地确定映射（或交换）哪些页，并比较进程之间的映射页。
-     */
+    
     if(GET_BIT(read_val, 63)) {
         uint64_t pfn = GET_PFN(read_val);
-        printf("PFN: 0x%llx (0x%llx)\n", pfn, pfn * page_size + virt_addr % page_size);
+        phys_addr = pfn * page_size + virt_addr % page_size;
+        printf("PFN: 0x%llx (0x%llx)\n", pfn, (unsigned long long) phys_addr);
     }
     else printf("Page not present\n");
     if(GET_BIT(read_val, 62)) printf("Page swapped\n");
     fclose(f);
+    return 0;
+}
+
+int write_var(enum CMD_TYPE type) {
+    printf("Ready to write in var\n\tparameterID: %d\n\ttarget phys addr: 0x%llx\n\tdata_size: %d\n", type, phys_addr, data_size);
+    
+    uint8_t cc[sizeof(uint64_t) + sizeof(uint64_t) + sizeof(uint64_t)];
+    memset(cc, 0, sizeof(cc));
+
+    // 填入type
+    uint64_t type_64 = (uint64_t) type;
+    memcpy(cc, &type_64, sizeof(uint64_t));
+
+    // 填入phys_addr
+    memcpy(cc + sizeof(uint64_t), &phys_addr, sizeof(uint64_t));
+
+    // 填入data_size
+    uint64_t data_size_64 = (uint64_t) data_size;
+    memcpy(cc + sizeof(uint64_t) + sizeof(uint64_t), &data_size_64, sizeof(uint64_t));
+
+    printf("cc array contents:\n");
+    for (size_t i = 0; i < sizeof(cc); ++i) {
+        printf("%02X ", cc[i]);
+    }
+    printf("\n");
+
+    char command[1024] = "./modify_var.sh \"";
+    char temp[5];
+
+    for (size_t i = 0; i < sizeof(cc); ++i) {
+        snprintf(temp, sizeof(temp), "\\x%02X", cc[i]);
+        strcat(command, temp);
+    }
+
+    strcat(command, "\"");
+
+    printf("Command: %s\n", command);
+
+    int result = system(command);
+    if (result != 0) {
+        fprintf(stderr, "Failed to execute script.\n");
+    }
+
     return 0;
 }
 
